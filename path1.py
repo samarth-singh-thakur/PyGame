76a13fc2e13190847a19e6bdf8968f5d27e8a0ee
violet360 <46649871+violet360@users.noreply.github.com>
2020-03-26 22:47:01 +0530

    Update path1.py

--- path1.py
+++ path1.py
@@ -9,21 +9,21 @@ class gt_grid():
 	def __init__(self):
 		self.start_r = 0
 		self.start_c = 0
-		self.end_r = 49
-		self.end_c = 49
-		self.col = 30
-		self.row = 30
+		self.end_r = 59
+		self.end_c = 59
+		self.col = 60
+		self.row = 60
 		self.walls = {}
 		self.t1 = [1, 0, -1, 0]
 		self.t2 = [0, 1, 0, -1]
-		self.vis = [[False for j in range(30)]for i in range(30)]
+		self.vis = [[False for j in range(60)]for i in range(60)]
 		self.The_list = []
 		self.flag = False
 		self.parent = {}
-		self.ways = [[False for j in range(30)]for i in range(30)]
-		self.dist = [[False for j in range(30)]for i in range(30)]
+		self.ways = [[False for j in range(60)]for i in range(60)]
+		self.dist = [[False for j in range(60)]for i in range(60)]
 		pygame.init()
-		self.screen = pygame.display.set_mode((300,300))
+		self.screen = pygame.display.set_mode((900,900))
 
 	def safe(self,r,c):
 		if (r>=0 and r<self.row and c>=0 and c<self.col):
@@ -35,27 +35,33 @@ class gt_grid():
 	def execute(self):
 		bb = self.bfs_util(self.start_r, self.start_c)
 		l = []
-		print(self.walls)
-
+		# print(self.walls)
+		r = 225
+		g = 30
 		for x in self.The_list:
 			o = x.split(',')
 			c0 = int(o[0])
 			r0 = int(o[1])
 
-			pygame.draw.rect(self.screen,(98,30,86),(c0*10,r0*10,9,9))
+			pygame.draw.rect(self.screen,(225,30,86),(c0*30,r0*30,30,30))
 			pygame.display.update()
+			time.sleep(.02)
+
+
+
 
 		if bb is True:
 			p = str(self.end_r)+","+str(self.end_c)
 			# print(p)
-			pygame.draw.rect(self.screen,(0, 255, 0),(self.end_c*10,self.end_r*10,9,9))
+			pygame.draw.rect(self.screen,(0, 255, 0),(self.end_c*30,self.end_r*30,30,30))
 			pygame.display.update()
+			# time.sleep()
 			while True:
 				try:
 					l.append(p)
 					c = int(p.split(",")[1])
 					r = int(p.split(",")[0])
-					pygame.draw.rect(self.screen,(0, 255, 0),(c*10,r*10,9,9))
+					pygame.draw.rect(self.screen,(0, 255, 0),(c*30,r*30,30,30))
 					pygame.display.update()
 					p = self.parent[p]
 					time.sleep(.12)
@@ -119,47 +125,47 @@ class gt_grid():
 
 				if pygame.mouse.get_pressed()[0]:
 					x,y = pygame.mouse.get_pos()
-					x = x//10
-					y = y//10
+					x = x//30
+					y = y//30
 		# print(pygame.key.get_pressed(), "*")
 						# print(x,y)
 					self.walls[str(x)+","+str(y)] = 1;
-					pygame.draw.rect(self.screen,c,(x*10,y*10,9,9))
+					pygame.draw.rect(self.screen,c,(x*30,y*30,30,30))
 
 
 				if event.type == KEYDOWN:
 					if event.key == K_s:
 							# print("Fvx")
 						x,y = pygame.mouse.get_pos()
-						x = x//10
-						y = y//10
+						x = x//30
+						y = y//30
 						# print(pygame.key.get_pressed(), "*")
 							# print(x,
 						self.start_r = y
 						self.start_c = x
-						print(y, x)
-						pygame.draw.rect(self.screen,start_clr,(x*10, y*10, 9, 9))
+						print(f"starting node -- {y}, {x}")
+						pygame.draw.rect(self.screen,start_clr,(x*30, y*30, 30, 30))
 						self.parent[str(y)+","+str(x)] = "-1"
 					elif event.key == K_e:
 	
 						x,y = pygame.mouse.get_pos()
-						x = x//10
-						y = y//10
+						x = x//30
+						y = y//30
 							# print(pygame.key.get_pressed(), "*")
 							# print(x,y)
 						self.end_r = y
 						self.end_c = x
-						print(y, x)
-						pygame.draw.rect(self.screen,end_clr,(x*10,y*10, 9, 9))
+						print(f"starting node -- {y}, {x}")
+						pygame.draw.rect(self.screen,end_clr,(x*30,y*30, 30, 30))
 
 					elif event.key == K_d:
 						return self.execute()
 				else:
 					break
 
-			for x in range (0,500,10):
-				pygame.draw.line(self.screen,c,(1,x),(300,x),2)
-				pygame.draw.line(self.screen,c,(x,1),(x,300), 2)
+			for x in range (0,900,30):
+				pygame.draw.line(self.screen,c,(1,x),(900,x),2)
+				pygame.draw.line(self.screen,c,(x,1),(x,900), 2)
 				pygame.display.update()
 
 
@@ -169,5 +175,6 @@ class gt_grid():
 
 
 obj = gt_grid()
-dist, ways = obj.init() #distance and number of ways
+ways, dist = obj.init() #distance and number of ways
+print(f"the shortest path to dest is {dist} blocks away and there are {ways} to reach the destination")
 
